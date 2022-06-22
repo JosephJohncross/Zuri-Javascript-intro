c4a6483526a4b4d544d520d6e231721662c13ddb
Joseph Ibochi <josephibochi@gmail.com>
2022-06-22 02:54:21 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,21 @@
+alert("A simple javascript calculator for two operands");
+let operation = prompt('Select a number to perform equivalent operation\n 1. +\n 2. *\n 3. -\n 4. /');
+
+let num1 = +prompt("enter first number", 0);
+let num2 =  +prompt("enter second second", 0);
+
+switch(operation){
+    case "1":
+        alert(`${num1 + num2}`);
+        break;
+    case "2":
+        alert(`${num1 * num2}`);
+        break;
+    case "3":
+        alert(`${num1 - num2}`);
+        break;
+    case "4":
+        alert(`${num1 / num2}`);
+        break;
+}
+
